0858a03ed06f8c7677788020a29bfa091c672525
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2024-04-18 11:03:24 +0530

    pr1_1.dart

--- demo1.dart
+++ demo1.dart
@@ -1,14 +1,23 @@
 import 'dart:io';
-
-void main() {
-  int n;
-
-  print("enter any value");
-  n = int.parse(stdin.readLineSync()!);
-
-  if (n > 0) {
-    print("number is positive");
-  } else {
-    print("number is nagetive");
+void main(){
+  var array = [];
+  List myArray = array.toList();
+  stdout.write("enter element your Array : ");
+  int len = int.parse(stdin.readLineSync()!);
+  for(int i = 1 ; i <= len ; i++){
+    stdout.write("Enter Element $i : ");
+    int el = int.parse(stdin.readLineSync()!);
+    myArray.add(el);
   }
+  print("Elements in the Array are : ");
+  myArray.forEach((element) {
+      stdout.write("$element   ");
+  });
+  print("");
+  print(" enter negative Elements in  Array  : ");
+  myArray.forEach((element) {
+    if(element<0){
+      stdout.write("$element   ");
+ }
+});
 }
