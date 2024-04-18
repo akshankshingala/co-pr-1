ef5867e95fbd9b6e42aeb8940f8b00b56de782b5
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2024-04-18 23:05:25 +0530

    Update demo1.dart

--- demo1.dart
+++ demo1.dart
@@ -2,10 +2,13 @@ import 'dart:io';
 void main(){
   var array = [];
   List myarray = array.toList();
+  
   stdout.write("enter element your Array : ");
   int len = int.parse(stdin.readLineSync()!);
+  
   for(int i = 1 ; i <= len ; i++){
     stdout.write("Enter Element $i : ");
+    
     int ele = int.parse(stdin.readLineSync()!);
     myarray.add(el);
   }
@@ -13,9 +16,11 @@ void main(){
   myarray.forEach((element) {
       stdout.write("$element   ");
   });
+  
   print("");
   print(" enter negative Elements in  Array  : ");
   myarray.forEach((element) {
+    
     if(element<0){
       stdout.write("$element   ");
  }
