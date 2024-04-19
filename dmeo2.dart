79f5dd67a4da344048977ccb4609c846eb9cba4d
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2024-04-19 09:19:23 +0530

    Update dmeo2.dart

--- dmeo2.dart
+++ dmeo2.dart
@@ -1,22 +1,25 @@
+/*
+ write a dart program to find largest number in array  
+*/
 import 'dart:io';
 
 void main() {
-  List<int> allNumbers = [];
+  List<int> Number = [];
 
-  print("Enter any 15 numbers: ");
+  print("Enter any 10 numbers: ");
 
-  for (int i = 0; i < 15; i++) {
-    String enterinput = (stdin.readLineSync()!);
-    int num = int.parse(enterinput);
-    allNumbers.add(num);
+  for (int i = 0; i < 10; i++) {
+    String a = stdin.readLineSync()!;
+    int n = int.parse(a);
+    Number.add(n);
   }
 
-  int bigetsNumber = allNumbers[0];
-  for (int i = 1; i < allNumbers.length; i++) {
-    if (allNumbers[i] > bigetsNumber) {
-      bigetsNumber = allNumbers[i];
+  int largestNumber = Number[0];
+  for (int i = 1; i < Number.length; i++) {
+    if (Number[i] > largestNumber) {
+      largestNumber = Number[i];
     }
   }
 
-  print('The largest number is: $bigetsNumber');
+  print('\nThe largest number is: $largestNumber');
 }
