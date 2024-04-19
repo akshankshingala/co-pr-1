7f6063abbbcb0efd59a80df06de92d8b2f21b23f
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2024-04-19 09:23:51 +0530

    Update demo4.dart

--- demo4.dart
+++ demo4.dart
@@ -1,37 +1,38 @@
+/*
+  
+  write a dart progrem to addition of two matries of dimension
+*/
+
+
+
 import 'dart:io';
-
-void main() {
-  List<List<int>> a = [
-    [1, 2, 3],
-    [7, 8, 9],
-    [4, 5, 6],
+void main(){
+  List<List<int>> Array1 = [
+    [11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19],
   ];
-  List<List<int>> b = [
-    [7, 8, 9],
-    [10, 11, 12],
-    [13, 14, 15],
+    List<List<int>> Array2 = [
+    [90, 80, 70],
+    [60, 50, 40],
+    [30, 20, 10],
   ];
-
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      stdout.write('Enter element of a[$i][$j] : ');
-      int val = int.parse(stdin.readLineSync()!);
-      a[i][j] = val;
-    }
-  }
-
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      stdout.write('Enter element of a[$i][$j] : ');
-      int val = int.parse(stdin.readLineSync()!);
-      a[i][j] = val;
-    }
-  }
-  print("Sum of Array:"); 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) stdout.write("${a[i][j] + b[i][j]} ");
-    print('');
-  }
+  print("\n Array 1 \n : ");
+  Array1.forEach((e) {
+    stdout.write("$e ");
+    print("");
+  });
+  print("\n Array 2 \n:");
+  Array2.forEach((e) {
+    stdout.write("$e ");
+    print("");
+  });
+  print("\nArray 1 + Array 2 :\n");
+  Array1.forEach((e) {
+    Array2.forEach((e) {
+      stdout.write("${e + e} ");
+      print("");
+     });
+  });
+  
 }
